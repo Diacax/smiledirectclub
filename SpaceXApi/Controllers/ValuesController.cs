ef9a4a1aac2961e5584fbb1c5eb816d37b32ec02
Diacax <deakes@gmail.com>
2018-08-01 00:04:02 -0500

    Add project files.

---
+++ SpaceXApi/Controllers/ValuesController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using SpaceXApi.Models;
+
+namespace SpaceXApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        static HttpClient client = new HttpClient();
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration) => _configuration = configuration;
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<LaunchpadModel>> GetAllLaunchPadData()
+        {
+            string launchpadData = null;
+            HttpResponseMessage response = await client.GetAsync(_configuration["SiteConfig"]);
+            if (response.IsSuccessStatusCode)
+            {
+                launchpadData = await response.Content.ReadAsStringAsync();
+                var productJObject = JArray.Parse(launchpadData).Select(x => new LaunchpadModel
+                {
+                    FullName = x["full_name"].ToString(),
+                    Id = x["id"].ToString(),
+                    Status = x["status"].ToString()
+                });
+                return productJObject;
+            }
+            return new List<LaunchpadModel>();
+        }
+
+        // GET api/values/id
+        [HttpGet("{id}")]
+        public async Task<object> GetById(string id)
+        {
+            string launchpadData = null;
+            HttpResponseMessage response = await client.GetAsync(_configuration["SiteConfig"] + "/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                launchpadData = await response.Content.ReadAsStringAsync();
+                JObject productJObject = JObject.Parse(launchpadData);
+                
+                LaunchpadModel launchpad = new LaunchpadModel
+                {
+                    FullName = productJObject["full_name"].ToString(),
+                    Id = productJObject["id"].ToString(),
+                    Status = productJObject["status"].ToString()
+                };
+                return launchpad;
+            }
+            return new List<LaunchpadModel>();
+        }
+        // GET api/values/status
+        [HttpGet("bystatus/{status}")]
+        public async Task<IEnumerable<LaunchpadModel>> GetAllLaunchPadByStatus(string status)
+        {
+            string launchpadData = null;
+            HttpResponseMessage response = await client.GetAsync(_configuration["SiteConfig"]);
+            if (response.IsSuccessStatusCode)
+            {
+                launchpadData = await response.Content.ReadAsStringAsync();
+                var productJObject = JArray.Parse(launchpadData).Where(y => (string)y["status"] == status).Select(x => new LaunchpadModel
+                {
+                    FullName = x["full_name"].ToString(),
+                    Id = x["id"].ToString(),
+                    Status = x["status"].ToString()
+                });
+                return productJObject;
+            }
+            return new List<LaunchpadModel>();
+        }
+    }
+}
---
+++ SpaceXApi/Models/launchpadModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceXApi.Models
+{
+    public class LaunchpadModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Status { get; set; }
+
+    }
+
+    public class Endpoint
+    {
+        public string DataLoc { get; set; }
+    }
+
+}
